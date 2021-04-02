a130886dd29a71cda0c693bc85d30492d4c139ca
ömer faruk semizoğlu <semizogluomerfaruk@outlook.com>
2021-04-02 23:41:18 +0300

    first commt

---
+++ BinanceBot/InnerOperation.cs
@@ -0,0 +1,60 @@
+﻿using Binance.API.Csharp.Client;
+using Binance.API.Csharp.Client.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace BinanceBot
+{
+    public class InnerOperation
+    {
+        Timer timer = new Timer();
+        int counter = 0;
+        public void Start()
+        {
+            WriteToFile("Service Başladı " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+
+        public void Stop()
+        {
+            WriteToFile("Service Durduruldu " + DateTime.Now);
+        }
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {           
+           WriteToFile("Binance Login :    " + DateTime.Now);
+        }
+    }
+}
