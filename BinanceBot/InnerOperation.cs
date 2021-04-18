8fe46a642a5f83d20ec34ea0a62d3be64dd6425b
Gökhan Ertaş <gokhan.ertas@outlook.com>
2021-04-18 09:41:52 +0300

    commit

--- BinanceBot/InnerOperation.cs
+++ BinanceBot/InnerOperation.cs
@@ -19,7 +19,7 @@ namespace BinanceBot
             WriteToFile("Service Başladı " + DateTime.Now);
             SendMessageFromTelegramBot("Servis çalışmaya başladı");
             timer.Elapsed += new ElapsedEventHandler(OnElapsedTimeAsync);
-            timer.Interval = 3600000;
+            timer.Interval = 400000; //3600000;
             timer.Enabled = true;
         }
 
