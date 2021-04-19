a1a75ec5ed96738c3136019e966afc500eab024d
Gökhan Ertaş <gokhan.ertas@outlook.com>
2021-04-19 22:48:28 +0300

    commit

--- BinanceBot/Program.cs
+++ BinanceBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Binance.Generate.OTT;
+using Binance.OTT.Trade;
 using System.ServiceProcess;
 
 namespace BinanceBot
@@ -8,39 +9,39 @@ namespace BinanceBot
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        //static void Main()
+        //{
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new Service1()
+            //};
+            //ServiceBase.Run(ServicesToRun);
+        //}
+
+
+        static async System.Threading.Tasks.Task Main()
         {
+#if (!DEBUG)
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
                 new Service1()
             };
             ServiceBase.Run(ServicesToRun);
+#else
+            Service1 myServ = new Service1();
+            //myServ.Process();
+
+
+            GenerateOTTLine.GenerateOTT();
+
+            await BinanceTrade.TradeAsync();
+
+            // here Process is my Service function
+            // that will run when my service onstart is call
+            // you need to call your own method or function name here instead of Process();
+#endif
         }
-
-
-//        static async System.Threading.Tasks.Task Main()
-//        {
-//#if (!DEBUG)
-//            ServiceBase[] ServicesToRun;
-//            ServicesToRun = new ServiceBase[]
-//            {
-//                new Service1()
-//            };
-//            ServiceBase.Run(ServicesToRun);
-//#else
-//            Service1 myServ = new Service1();
-//            //myServ.Process();
-
-
-//            GenerateOTTLine.GenerateOTT();
-
-//            await BinanceTrade.TradeAsync();
-
-//            // here Process is my Service function
-//            // that will run when my service onstart is call
-//            // you need to call your own method or function name here instead of Process();
-//#endif
-//        }
     }
 }
