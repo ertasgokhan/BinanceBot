64910ad5581ffdae0ea4dd65614b49c344981bd5
Gökhan Ertaş <gokhan.ertas@outlook.com>
2021-04-24 21:32:44 +0300

    commit

--- BinanceBot/ProjectInstaller.Designer.cs
+++ BinanceBot/ProjectInstaller.Designer.cs
@@ -53,6 +53,7 @@
             // 
             // serviceProcessInstaller1
             // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
             this.serviceProcessInstaller1.Password = null;
             this.serviceProcessInstaller1.Username = null;
             // 
